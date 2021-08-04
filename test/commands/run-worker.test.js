9dc4e8b142c01cf2a87ebd70b912130b1de65a8e
delbick <delbick@adobe.com>
2021-08-03 17:02:45 -0700

    check standard out

--- test/commands/run-worker.test.js
+++ test/commands/run-worker.test.js
@@ -170,9 +170,7 @@ describe("run-worker command", function() {
                 process.env.WORKER_DEBUG = "myworker";
             })
             .it("passes WORKER_DEBUG env var through as DEBUG", function(ctx) {
-                console.log("~~~~~~~~~~~~~ STDERR", ctx.stderr);
-                console.log("~~~~~~~~~~~~~ STDOUT", ctx.stdout);
-                assert(ctx.stderr.includes(">>>> debug log is here <<<<"));
+                assert(ctx.stderr.includes(">>>> debug log is here <<<<") || ctx.stdout.includes(">>>> debug log is here <<<<"));
             });
     });
 
